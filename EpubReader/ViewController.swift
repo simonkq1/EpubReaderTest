26551b1dfca999f33f4bce9bf6d4071aa10776f9
simonkq1 <simonkq1@gmail.com>
2019-03-06 09:00:44 +0800

    Initial Commit

---
+++ EpubReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EpubReader
+//
+//  Created by Jetec-RD on 2019/3/6.
+//  Copyright © 2019 JETEC ELETRONICS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ EpubReaderTests/EpubReaderTests.swift
@@ -0,0 +1,34 @@
+//
+//  EpubReaderTests.swift
+//  EpubReaderTests
+//
+//  Created by Jetec-RD on 2019/3/6.
+//  Copyright © 2019 JETEC ELETRONICS. All rights reserved.
+//
+
+import XCTest
+@testable import EpubReader
+
+class EpubReaderTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
