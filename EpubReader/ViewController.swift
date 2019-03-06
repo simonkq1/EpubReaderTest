ed5c9445f55235a4f3b53f0f8d17263ca2d8d26b
simonkq1 <simonkq1@gmail.com>
2019-03-06 17:55:40 +0800

    3/6

--- EpubReader/ViewController.swift
+++ EpubReader/ViewController.swift
@@ -7,12 +7,63 @@
 //
 
 import UIKit
+import SSZipArchive
 
 class ViewController: UIViewController {
 
+    let fm = FileManager.default
+    let home = NSHomeDirectory()
+    let tmp = NSHomeDirectory() + "/tmp/"
+    let fileName = "e-book-1"
+    let fileType = "epub"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let path = home + "/tmp/" + getFileName(fileName, fileType)
+        copyEpub(to: path)
+        readEpub(from: path)
+        do {
+            guard let texts = try? fm.contentsOfDirectory(atPath: tmp + fileName + "/OEBPS/Text") else {return}
+            for i in texts {
+                print(i)
+            }
+            
+            guard let images = try? fm.contentsOfDirectory(atPath: tmp + fileName + "/OEBPS/Images") else {return}
+            for i in images {
+                print(i)
+            }
+            guard let string = try? String(contentsOfFile: tmp + fileName + "/OEBPS/Text/Section0002.xhtml") else {return}
+            print(string)
+        }
+    }
+    
+    func copyEpub(to path: String) {
+        guard let file = Bundle.main.path(forResource: fileName, ofType: fileType) else {print("Epub not find"); return}
+        guard !fm.fileExists(atPath: path) else {print("File already exist"); return}
+        do {
+            try fm.copyItem(at: URL(fileURLWithPath: file), to: URL(fileURLWithPath: path))
+        }catch {
+            print(error)
+        }
+    }
+    
+    func readEpub(from path: String) {
+        let zipPath = home + "/tmp/" + fileName
+        guard fm.fileExists(atPath: path), !fm.fileExists(atPath: zipPath) else {print("File not exist"); return}
+        if SSZipArchive.unzipFile(atPath: tmp + getFileName(fileName, fileType), toDestination: tmp + fileName) {
+            do {
+                guard let files = try? fm.contentsOfDirectory(atPath: tmp + fileName + "/OEBPS") else {return}
+                for i in files {
+                    print(i)
+                }
+                
+            }
+        }
+    }
+    
+    func getFileName(_ name: String, _ type: String) -> String {
+        return name + "." + type
     }
 
 
---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'EpubReader' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+  pod 'FolioReaderKit'
+
+  # Pods for EpubReader
+
+  target 'EpubReaderTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'EpubReaderUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
